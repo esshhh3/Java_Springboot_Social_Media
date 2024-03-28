bc57a254dfc2a362be430d5bcaf6e704dddd2bb2
AdityaDhanaraj <adityadhanarajkundu@gmail.com>
2024-03-28 20:45:59 +0530

    basic initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Authentication from './Pages/Authentication/Authentication'
+import Login from './Pages/Authentication/Login'
+import Register from './Pages/Authentication/Register'
+
+function App() {
+
+  return (
+    <div>
+      <Authentication />
+      <Login />
+      <Register />
+    </div>
+  )
+}
+
+export default App
